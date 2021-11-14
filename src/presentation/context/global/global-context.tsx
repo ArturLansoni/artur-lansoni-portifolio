6ae4dfd4f681afd291c4349fc01f2933b90ad3eb
Artur Lansoni <arturlansoni@gmail.com>
2021-11-14 20:03:28 -0300

    feat: create pages and context

---
+++ src/presentation/context/global/global-context.tsx
@@ -0,0 +1,55 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { SeoModel } from '@/domain/models'
+
+type StateProps = {
+	defaultSeo?: SeoModel
+	siteName: string
+	translationUrl?: string
+}
+
+type Props = StateProps & {
+	setTranslationUrl: (param: string) => void
+}
+type ProviderProps = {
+	children: React.ReactNode
+	initialProps: {
+		defaultSeo?: SeoModel
+		siteName: string
+	}
+}
+
+const GlobalContext = createContext<Props>(null)
+
+function GlobalProvider({ children, initialProps }: ProviderProps) {
+	const [state, setState] = useState<StateProps>({
+		defaultSeo: null,
+		siteName: '',
+		translationUrl: ''
+	})
+
+	useEffect(() => {
+		setState(old => ({
+			...old,
+			...initialProps
+		}))
+	}, [])
+
+	const setTranslationUrl = (value: string): void =>
+		setState(old => ({ ...old, translationUrl: value }))
+
+	return (
+		<GlobalContext.Provider value={{ ...state, setTranslationUrl }}>
+			{children}
+		</GlobalContext.Provider>
+	)
+}
+
+function useGlobal(): Props {
+	const context = useContext(GlobalContext)
+	if (!context) {
+		throw new Error('useGlobal must be used within an GlobalProvider.')
+	}
+	return context
+}
+
+export { GlobalProvider, useGlobal }
---
+++ src/presentation/pages/home/home.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { SeoModel } from '@/domain/models'
+import { Formatter } from '@/data/protocols'
+import { Error, Seo, ArticleCard } from '@/presentation/components'
+import { LoadArticleList } from '@/domain/use-cases'
+import { Flex, Grid } from '@chakra-ui/react'
+
+type Props = {
+	articles: LoadArticleList.Model[]
+	error?: string
+	seo: SeoModel
+	publishedDateFormatter: Formatter<{ date: Date; locale?: string }, string>
+}
+
+function Home({ articles = [], error, seo, publishedDateFormatter }: Props) {
+	function ShowCurrentState() {
+		if (error) return <Error error={error} reload={() => {}} />
+		return (
+			<Grid as='ul' w='100%' templateColumns='repeat(auto-fit, minmax(320px, 1fr))' gap={5}>
+				{articles.map((article: LoadArticleList.Model) => (
+					<Flex as='li' listStyleType='none' key={article.id}>
+						<ArticleCard
+							article={article}
+							publishedDateFormatter={publishedDateFormatter}
+						/>
+					</Flex>
+				))}
+			</Grid>
+		)
+	}
+
+	return (
+		<>
+			<Seo params={seo} isArticle={false} />
+			<ShowCurrentState />
+		</>
+	)
+}
+
+export default Home
