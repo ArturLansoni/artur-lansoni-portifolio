0909f1d06d1df2ad10dd3e8562b03481e1d83ba4
Artur Lansoni <arturlansoni@gmail.com>
2021-11-15 02:38:48 -0300

    chore: project configs

--- i18n.js
+++ i18n.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 module.exports = {
 	locales: ['en', 'pt'],
 	defaultLocale: 'en',
--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate')
 
